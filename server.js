81f41a739c14acfa6b4686944a102b082c6975e5
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-09-14 22:07:18 +0000

    added a create method

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const port=8000;
-const app= express();
+const app = express();
 
 const User=require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
-
+mongoose.connect('mongodb://localhost/userData', {useNewUrlParser: true,useUnifiedTopology: true})
+// const db= mongoose.connect('mongodb://localhost/bookAPI', {useNewUrlParser: true,useUnifiedTopology: true});
 app.use(bodyParser.json());
 
 
@@ -16,7 +16,21 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
 app.route('/users/:id')
